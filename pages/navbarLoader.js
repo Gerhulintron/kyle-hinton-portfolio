ddbd478beb9e5cc08f8ad10e1d03d80285f78af3
Gerhulintron <kyle-hinton@hotmail.com>
2025-06-10 17:52:14 -0600

    Additionaly fix to the navbar management

--- pages/navbarLoader.js
+++ pages/navbarLoader.js
@@ -4,41 +4,6 @@ fetch("pages/KyleHintonProjectsNavbar.html")
     .then(data => {
         document.getElementById('navbar-container').innerHTML = data;
 
-        const isInPagesDir = window.location.pathname.includes('/pages/');
-        const prefix = isInPagesDir ? './' : 'pages/';
-        const homePath = isInPagesDir ? '../index.html' : 'index.html';
-
-        document.querySelectorAll('[data-rel]').forEach(link => {
-            const rel = link.getAttribute('data-rel');
-            if (rel === 'home' || rel === 'portfolio') {
-                link.setAttribute('href', homePath);
-            } else if (rel) {
-                link.setAttribute('href', `${prefix}${capitalize(rel)}.html`);
-            }
-        });
-
-        function capitalize(str) {
-            return str.charAt(0).toUpperCase() + str.slice(1);
-        }
-
-        const navbar = document.querySelector('.navbar');
-        if (navbar) {
-            const spotlight = document.createElement('div');
-            spotlight.classList.add('spotlight');
-            navbar.appendChild(spotlight);
-
-            navbar.addEventListener('mousemove', function (e) {
-                spotlight.style.display = 'block';
-                const x = e.clientX - navbar.getBoundingClientRect().left;
-                const y = e.clientY - navbar.getBoundingClientRect().top;
-                spotlight.style.transform = `translate(${x}px, ${y}px)`;
-            });
-
-            navbar.addEventListener('mouseleave', function () {
-                spotlight.style.display = 'none';
-            });
-        }
-
         window.toggleMobileMenu = function () {
             const menu = document.getElementById("mobileMenu");
             if (menu) {
@@ -46,3 +11,4 @@ fetch("pages/KyleHintonProjectsNavbar.html")
             }
         };
     });
+
